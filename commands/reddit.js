2090c80f8b9a15451bf4c79d4c48827fa92da729
Giorgio Productions <giorgioproductions@gmail.com>
2020-09-27 13:46:52 +0200

    improved Reddit command

--- commands/reddit.js
+++ commands/reddit.js
@@ -9,27 +9,28 @@ module.exports = {
     const isnsfw = nsfw.find((c) => c.includes(command));
     if (isnsfw) {
       if (message.channel.nsfw) {
-        getReddit(command, message.channel);
+        getReddit(command, message);
       }
       else {
         message.channel.send("Este comando solo puede ser enviado a un canal NSFW.");
       }
     }
     else {
-      getReddit(command, message.channel);
-    }
+      getReddit(command, message);
+	}
+	message.delete();
   }
 }
 
-function getReddit(query, channel) {
+function getReddit(query, message) {
   if (query == "yuri") {query = "wholesomeyuri"}
 	fetch('https://www.reddit.com/r/' + query + '.json?limit=100&?sort=top&t=all')
     .then(res => res.json())
     .then(json => json.data.children.map(v => v.data.url))
-    .then(urls => postReddit(query, channel, urls));
+    .then(urls => postReddit(query, message, urls));
 }
 
-function postReddit(query, channel, urls) {
+function postReddit(query, message, urls) {
 	const randomURL = urls[Math.floor(Math.random() * urls.length) + 1];
 	try {
 		if (randomURL.endsWith("gif") || randomURL.endsWith("jpg") || randomURL.endsWith("png")) {
@@ -39,18 +40,19 @@ function postReddit(query, channel, urls) {
 			.setColor('#DB8E4A')
 			.setURL(randomURL)
 			.setTitle(randomURL)
+			.setDescription("Solicitado por "+message.author.toString())
 			.setFooter("Si dos personas pulsan la ❌ la imagen se borrará.");
-				channel.send(embed).then(sentEmbed => {
+				message.channel.send(embed).then(sentEmbed => {
 		    sentEmbed.react("❤️");
 				sentEmbed.react("❌");
 			});
 		} else {
 			console.log('Image format is not supported. Choosing another image...');
-			postReddit(query, channel, urls);
+			postReddit(query, message, urls);
 		}
 	} catch (e) {
 			console.log('Image not found. Retrying...');
-			postReddit(query, channel, urls);
+			postReddit(query, message, urls);
 	}
 
 }
