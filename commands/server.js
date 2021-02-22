4a15703a435c14b08c1b1e6665fa724efaad662c
GiorgioProductions <giorgioproductions@gmail.com>
2021-02-22 04:23:03 +0100

    removed unnecessary files

--- commands/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "server",
-  alias: ["sv"],
-  run: (client, message, command, args, emojis, con) => {
-    if (message.member.hasPermission("ADMINISTRATOR")) {
-      return message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    }
-  }
-}
--- commands/taploop.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: "taploop",
-  alias: ["taploop"],
-  run: (client, message, command, args, emojis, con) => {
-    message.channel.send(`¡Descarga TapLoop! ${taploop}\nhttps://play.google.com/store/apps/details?id=com.giorgioproductions.gmbeat&hl=es`);
-  }
-}
